afe123e3f06a22ed3455b661005dce8d0c90f862
ahmod001 <ahmodh08@gmail.com>
2023-09-18 16:30:17 +0600

    eduction

--- resources/views/components/education.blade.php
+++ resources/views/components/education.blade.php
@@ -1,17 +1,57 @@
 <div class="container px-5 my-5">
     <div class="text-center mb-5">
-        
+
     </div>
     <div class="row gx-5 justify-content-center">
         <div class="col-lg-11 col-xl-9 col-xxl-8">
             <section id="Education-section">
-                <h2  class="text-secondary fw-bolder mb-4">Education</h2>
+                <h2 class="text-secondary fw-bolder mb-4">Education</h2>
                 <!-- Education Card 1-->
 
-            
-               
+
+
             </section>
-            
+
         </div>
     </div>
 </div>
+
+
+<script>
+    getEducation();
+    async function getEducation() {
+
+        try {
+            const response = await axios.get('/educationData');
+
+            response.data.forEach(item => {
+                document.getElementById('Education-section').innerHTML += (
+                    ` <div class="card shadow border-0 rounded-4 mb-5">
+                                <div class="card-body p-5">
+                                    <div class="row align-items-center gx-5">
+                                        <div class="col text-center text-lg-start mb-4 mb-lg-0">
+                                            <div class="bg-light p-4 rounded-4">
+                                                <div class="text-secondary fw-bolder mb-2">${item['duration']}</div>
+                                                <div class="mb-2">
+                                                    <div class="small fw-bolder">${item['institutionName']}</div>
+                                                  
+                                                </div>
+                                                <div class="fst-italic">
+                                                    <div class="small text-muted">${item['field']}</div>
+                                                    
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="col-lg-8"><div>${item['details']}</div></div>
+                                    </div>
+                                </div>
+                            </div>`)
+
+            });
+
+        } catch (error) {
+            alert('something went wrong')
+            console.error(error);
+        }
+    }
+</script>
